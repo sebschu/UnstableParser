ee4d79442829363b22fb5a5fefdd8a5138cffc9a
Sebastian Schuster <sebschu@gmail.com>
2017-06-14 17:00:29 -0700

    Add server so that the parser can be run as a webservice with CoreNLP.

--- parser/network.py
+++ parser/network.py
@@ -2,13 +2,13 @@
 # -*- coding: UTF-8 -*-
 
 # Copyright 2016 Timothy Dozat
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -38,16 +38,16 @@ from parser.neural.optimizers import RadamOptimizer
 #***************************************************************
 class Network(Configurable):
   """"""
-  
+
   #=============================================================
   def __init__(self, *args, **kwargs):
     """"""
-    
+
     super(Network, self).__init__(*args, **kwargs)
     # hacky!
     #hacky_train_files = op.join(self.save_dir, op.basename(self.get("train_files")))
     #self._config.set('Configurable', 'train_files', hacky_train_files)
-    
+
     # TODO make this more flexible, maybe specify it in config?
     temp_nlp_model = self.nlp_model.from_configurable(self)
     if temp_nlp_model.input_vocabs == ['tags']:
@@ -71,11 +71,11 @@ class Network(Configurable):
     self._global_epoch = tf.Variable(0., trainable=False, name='global_epoch')
     self._optimizer = RadamOptimizer.from_configurable(self, global_step=self.global_step)
     return
-  
+
   #=============================================================
   def add_file_vocabs(self, conll_files):
     """"""
-    
+
     # TODO don't depend on hasattr
     for vocab in self.vocabs:
       if hasattr(vocab, 'add_files'):
@@ -84,11 +84,11 @@ class Network(Configurable):
       if hasattr(vocab, 'index_tokens'):
         vocab.index_tokens()
     return
-  
+
   #=============================================================
   def train(self, load=False):
     """"""
-    
+
     # prep the configurables
     self.add_file_vocabs(self.parse_files)
     trainset = Trainset.from_configurable(self, self.vocabs, nlp_model=self.nlp_model)
@@ -106,7 +106,7 @@ class Network(Configurable):
     best_acc = 0
     n_iters_since_improvement = 0
     n_iters_in_epoch = 0
-    
+
     # calling these properties is inefficient so we save them in separate variables
     min_train_iters = self.min_train_iters
     max_train_iters = self.max_train_iters
@@ -114,13 +114,13 @@ class Network(Configurable):
     save_every = self.save_every
     verbose = self.verbose
     quit_after_n_iters_without_improvement = self.quit_after_n_iters_without_improvement
-    
+
     # load or prep the history
     if load:
       self.history = pkl.load(open(os.path.join(self.save_dir, 'history.pkl')))
     else:
       self.history = {'train': defaultdict(list), 'valid': defaultdict(list)}
-    
+
     # start up the session
     config_proto = tf.ConfigProto()
     if self.per_process_gpu_memory_fraction == -1:
@@ -164,7 +164,7 @@ class Network(Configurable):
             current_acc = validset.update_history(self.history['valid'], valid_accumulators)
             # print
             if verbose:
-              print(ctext('{0:6d}'.format(int(total_train_iters)), 'bold')+')') 
+              print(ctext('{0:6d}'.format(int(total_train_iters)), 'bold')+')')
               trainset.print_accuracy(train_accumulators, train_time)
               validset.print_accuracy(valid_accumulators, valid_time)
             train_accumulators = np.zeros(len(train_outputs))
@@ -204,7 +204,7 @@ class Network(Configurable):
         input_dir, input_file = os.path.split(input_file)
         output_dir = self.save_dir
         output_file = input_file
-        
+
         start_time = time.time()
         probs = []
         sents = []
@@ -215,17 +215,17 @@ class Network(Configurable):
     if self.verbose:
       print(ctext('Parsing {0} file(s) took {1} seconds'.format(len(input_files), time.time()-start_time), 'bright_green'))
     return
-  
+
   #=============================================================
   def parse(self, input_files, output_dir=None, output_file=None):
     """"""
-    
+
     if not isinstance(input_files, (tuple, list)):
       input_files = [input_files]
     if len(input_files) > 1 and output_file is not None:
       raise ValueError('Cannot provide a value for --output_file when parsing multiple files')
     self.add_file_vocabs(input_files)
-    
+
     # load the model and prep the parse set
     trainset = Trainset.from_configurable(self, self.vocabs, nlp_model=self.nlp_model)
     with tf.variable_scope(self.name.title()):
@@ -242,7 +242,7 @@ class Network(Configurable):
       for var in self.non_save_vars:
         sess.run(var.initializer)
       saver.restore(sess, tf.train.latest_checkpoint(self.save_dir))
-      
+
       # Iterate through files and batches
       for input_file in input_files:
         parseset = Parseset.from_configurable(trainset, self.vocabs, parse_files=input_file, nlp_model=self.nlp_model)
@@ -259,7 +259,7 @@ class Network(Configurable):
           output_path = os.path.join(output_dir, input_file)
         else:
           output_path = output_file
-        
+
         start_time = time.time()
         probs = []
         sents = []
@@ -270,7 +270,57 @@ class Network(Configurable):
     if self.verbose:
       print(ctext('Parsing {0} file(s) took {1} seconds'.format(len(input_files), time.time()-start_time), 'bright_green'))
     return
-  
+
+  def preload(self):
+    '''
+       Preloads the model into memory, so that it can be used by parse_online.
+    '''
+
+
+    self.add_file_vocabs(self.train_files)
+
+    # load the model and prep the parse set
+    self.preloaded_trainset = Trainset.from_configurable(self, self.vocabs, nlp_model=self.nlp_model)
+    with tf.variable_scope(self.name.title()):
+      train_tensors = self.preloaded_trainset()
+    train_outputs = [train_tensors[train_key] for train_key in self.preloaded_trainset.train_keys]
+
+    saver = tf.train.Saver(self.save_vars, max_to_keep=1)
+    config_proto = tf.ConfigProto(allow_soft_placement=True)
+    if self.per_process_gpu_memory_fraction == -1:
+      config_proto.gpu_options.allow_growth = True
+    else:
+      config_proto.gpu_options.per_process_gpu_memory_fraction = self.per_process_gpu_memory_fraction
+    self.session_server = tf.train.Server.create_local_server(config=config_proto)
+    with tf.Session(self.session_server.target) as sess:
+      for var in self.non_save_vars:
+        sess.run(var.initializer)
+      saver.restore(sess, tf.train.latest_checkpoint(self.save_dir))
+      if self.verbose:
+        print(ctext('Preloaded model', 'bright_green'))
+    return
+
+
+  def parse_online(self, input_file):
+    input_files = [input_file]
+    self.add_file_vocabs(input_files)
+
+    with tf.Session(self.session_server.target) as sess:
+      parseset = Parseset.from_configurable(self.preloaded_trainset, self.vocabs, parse_files=input_file, nlp_model=self.nlp_model)
+      with tf.variable_scope(self.name.title(), reuse=True):
+        parse_tensors = parseset(moving_params=self.optimizer)
+      parse_outputs = [parse_tensors[parse_key] for parse_key in parseset.parse_keys]
+
+      probs = []
+      sents = []
+      for feed_dict, tokens in parseset.iterbatches(shuffle=False):
+        probs.append(sess.run(parse_outputs, feed_dict=feed_dict))
+        sents.append(tokens)
+      if self.verbose:
+        print(ctext('Successfully parsed sentences.', 'bright_green'))
+      return sents, probs, parseset
+
+
   #=============================================================
   @property
   def vocabs(self):
@@ -297,7 +347,7 @@ class Network(Configurable):
 #***************************************************************
 if __name__ == '__main__':
   """"""
-  
+
   from parser import Network
   configurable = Configurable()
   network = Network.from_configurable(configurable)
---
+++ server.py
@@ -0,0 +1,169 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+# Copyright 2016 Timothy Dozat, Sebastian Schuster (the server component)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import io
+import re
+import os
+import sys
+import codecs
+from argparse import ArgumentParser
+
+import numpy as np
+
+from flask import Flask, jsonify, request, Response
+from corenlp_protobuf import Document, parseFromDelimitedString, writeToDelimitedString
+
+from parser.misc.colors import ctext
+from parser.misc.mst import nonprojective, argmax
+
+from parser import Configurable
+from parser import Network
+
+network =  None
+
+# TODO make the pretrained vocab names a list given to TokenVocab
+#***************************************************************
+# Set up the argparser
+argparser = ArgumentParser('Network')
+argparser.add_argument('--save_dir', required=True)
+subparsers = argparser.add_subparsers()
+section_names = set()
+# --section_name opt1=value1 opt2=value2 opt3=value3
+with codecs.open('config/defaults.cfg') as f:
+  section_regex = re.compile('\[(.*)\]')
+  for line in f:
+    match = section_regex.match(line)
+    if match:
+      section_names.add(match.group(1).lower().replace(' ', '_'))
+
+
+
+#=================================================================
+# Web service
+#-----------------------------------------------------------------
+app = Flask(__name__)
+
+@app.route('/ping/')
+def handle_ping():
+  return "pong"
+
+@app.route('/annotate/',  methods=['POST'])
+def annotate():
+  msg = request.data
+  # Do the annotation
+  doc = Document()
+  parseFromDelimitedString(doc, msg)
+
+  input_file = "/tmp/annotator_in.conllu"
+  f = codecs.open(input_file, 'w', encoding='utf-8', errors='ignore')
+  for sentence in doc.sentence:
+    f.write(_to_conllu(sentence))
+  f.close()
+
+  sents, probs, parseset = network.parse_online(input_file)
+  _fill_parse_annotations(doc, probs, parseset)
+
+  with io.BytesIO() as stream:
+    writeToDelimitedString(doc, stream)
+    msg = stream.getvalue()
+
+  return Response(msg, mimetype="application/x-protobuf")
+
+def _to_conllu(sentence_ann):
+  conllu_lines = []
+  for idx, token in enumerate(sentence_ann.token):
+    lemma = token.lemma if len(token.lemma) > 0 else  "_"
+    uPOS = token.coarseTag if len(token.coarseTag) > 0 else "_"
+    conllu_string = "%d\t%s\t%s\t%s\t%s\t_\t_\t_\t_\t_" % ((idx + 1),
+                                                           token.word,
+                                                           lemma,
+                                                           uPOS,
+                                                           token.pos)
+    conllu_lines.append(conllu_string)
+
+  #append extra empty line at the end
+  conllu_lines.append("\n")
+  return "\n".join(conllu_lines)
+
+def _fill_parse_annotations(ann, probs, parseset):
+  arc_probs = [arc_prob for batch in probs for arc_prob in batch[0]]
+  rel_probs = [rel_prob for batch in probs for rel_prob in batch[1]]
+  tokens_to_keep = [weight for batch in probs for weight in batch[2]]
+
+  j = 0
+  for i in parseset.multibucket.inv_idxs():
+    #TODO: figure out why this happens
+    if j >= len(ann.sentence):
+      break
+    sentence = ann.sentence[j]
+    arc_prob, rel_prob, weights = arc_probs[i], rel_probs[i], tokens_to_keep[i]
+    sequence_length = int(np.sum(weights))+1
+    arc_prob = arc_prob[:sequence_length][:,:sequence_length]
+    arc_preds = nonprojective(arc_prob)
+    arc_preds_one_hot = np.zeros([rel_prob.shape[0], rel_prob.shape[2]])
+    arc_preds_one_hot[np.arange(len(arc_preds)), arc_preds] = 1.
+    rel_preds = np.argmax(np.einsum('nrb,nb->nr', rel_prob, arc_preds_one_hot), axis=1)
+    for token, arc_pred, rel_pred, weight in zip(sentence.token, arc_preds[1:], rel_preds[1:], weights[1:]):
+      token.conllUMisc = "{0}:{1}".format(parseset._nlp_model.vocabs['heads'][arc_pred], parseset._nlp_model.vocabs['rels'][rel_pred])
+    j += 1
+
+
+#================================================================
+# Parse server/annotator
+#----------------------------------------------------------------
+
+def parse_server(save_dir, **kwargs):
+  global network
+  kwargs['config_file'] = os.path.join(save_dir, 'config.cfg')
+  kwargs['is_evaluation'] = True
+  network = Network(**kwargs)
+  network.preload()
+
+  print(ctext('Starting annotation server...', 'bright_green'))
+  app.run()
+
+  return
+
+#----------------------------------------------------------------
+
+parse_server_parser = subparsers.add_parser('parse_server')
+parse_server_parser.set_defaults(action=parse_server)
+for section_name in section_names:
+  parse_server_parser.add_argument('--'+section_name, nargs='+')
+parse_server_parser.add_argument('--output_file')
+parse_server_parser.add_argument('--output_dir')
+
+
+
+#***************************************************************
+# Parse the arguments
+kwargs = vars(argparser.parse_args())
+action = kwargs.pop('action')
+save_dir = kwargs.pop('save_dir')
+kwargs = {key: value for key, value in kwargs.iteritems() if value is not None}
+for section, values in kwargs.iteritems():
+  if section in section_names:
+    values = [value.split('=', 1) for value in values]
+    kwargs[section] = {opt: value for opt, value in values}
+if 'default' not in kwargs:
+  kwargs['default'] = {}
+kwargs['default']['save_dir'] = save_dir
+action(save_dir, **kwargs)
